21bd892f83c95349a001d91ac20e2d1206684db1
Artem Filippow <a.filippov@qtech.ru>
2025-01-27 17:46:10 +0900

    Functions description

--- src/widget.py
+++ src/widget.py
@@ -3,17 +3,16 @@ from masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(info: str) -> str:
+    """Маскирует номер карты или счёта"""
     card_info = info.split()
     print(card_info)
     if len(card_info[-1]) == 16:
         card_info[-1] = get_mask_card_number(int(card_info[-1]))
     else:
         card_info[-1] = get_mask_account(int(card_info[-1]))
-    return ' '.join(card_info)
+    return " ".join(card_info)
 
 
 def get_date(date: str) -> str:
-    return parse(date).strftime('%d.%m.%Y')
-
-
-
+    """Форматирует дату"""
+    return parse(date).strftime("%d.%m.%Y")
