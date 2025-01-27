167c54d26a4a99d1f76c5fe2947f21c19eb3a3a0
Artem Filippow <a.filippov@qtech.ru>
2025-01-27 17:45:24 +0900

    lint code

--- src/masks.py
+++ src/masks.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 def get_mask_card_number(card_number: int) -> str:
-    '''принимает на вход номер карты и возвращает ее маску'''
+    """принимает на вход номер карты и возвращает ее маску"""
     if len(str(card_number)) != 16:
         return "Wrong card number"
     card_number_str = str(card_number)
@@ -10,8 +10,9 @@ def get_mask_card_number(card_number: int) -> str:
 
 
 def get_mask_account(account_number: int) -> str:
-    '''принимает на вход номер счета и возвращает его маску'''
+    """принимает на вход номер счета и возвращает его маску"""
     account_number_str = str(account_number)
     return f"**{account_number_str[-4:]}"
+
 
 # print(get_mask_card_number(1234562289606361))
