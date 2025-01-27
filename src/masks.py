d655367b272c2b7a9bb29dbd9586aee4e91474e4
Artem Filippow <a.filippov@qtech.ru>
2025-01-27 17:57:52 +0900

    refactoring functions for string params

--- src/masks.py
+++ src/masks.py
@@ -1,18 +1,15 @@
 from typing import List
 
 
-def get_mask_card_number(card_number: int) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """принимает на вход номер карты и возвращает ее маску"""
-    if len(str(card_number)) != 16:
+    if len(card_number) != 16:
         return "Wrong card number"
-    card_number_str = str(card_number)
-    return f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
-def get_mask_account(account_number: int) -> str:
+def get_mask_account(account_number: str) -> str:
     """принимает на вход номер счета и возвращает его маску"""
-    account_number_str = str(account_number)
-    return f"**{account_number_str[-4:]}"
-
+    return f"**{account_number[-4:]}"
 
 # print(get_mask_card_number(1234562289606361))
