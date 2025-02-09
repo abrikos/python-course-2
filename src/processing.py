19b863b6c0fedb485d59da886598209872125c7c
Artem Filippow <a.filippov@qtech.ru>
2025-02-09 10:25:26 +0900

    sort by date function

--- src/processing.py
+++ src/processing.py
@@ -5,3 +5,5 @@ def filter_by_state(dictionaries: List, state: str = 'EXECUTED') -> List:
     return list(filter(lambda item: item['state'] == state, dictionaries))
 
 
+def sort_by_date(dictionaries: List, sort: str = 'ASC') -> List:
+    return sorted(dictionaries, key=lambda item: item['date'], reverse=sort != 'ASC')
