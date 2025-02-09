26a533416917cf1c18666ec13de78bb95996a550
Artem Filippow <a.filippov@qtech.ru>
2025-02-09 10:29:28 +0900

    descriptions for functions

--- src/processing.py
+++ src/processing.py
@@ -2,8 +2,10 @@ from typing import List
 
 
 def filter_by_state(dictionaries: List, state: str = 'EXECUTED') -> List:
+    """Фильтрация списков по полю state"""
     return list(filter(lambda item: item['state'] == state, dictionaries))
 
 
 def sort_by_date(dictionaries: List, sort: str = 'ASC') -> List:
+    """Сортировка списка по дате"""
     return sorted(dictionaries, key=lambda item: item['date'], reverse=sort != 'ASC')
