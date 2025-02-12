2cc49c290ea46503d10217b02415a0d5a7882a1d
Artem Filippow <a.filippov@qtech.ru>
2025-02-12 18:40:04 +0900

    refactoring masks module for check type of inputs

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,19 @@
-def get_mask_card_number(card_number: str) -> str:
+import re
+
+
+def get_mask_card_number(card_number: str) -> str | bool:
     """принимает на вход номер карты и возвращает ее маску"""
-    if len(card_number) != 16:
-        return "Wrong card number"
-    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    if not type(card_number) is str:
+        return False
+
+    if not re.match(r'\d{4} \d{4} \d{4} \d{4}', card_number):
+        return False
+    return f"{card_number[:4]} {card_number[5:7]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(account_number: str) -> str:
     """принимает на вход номер счета и возвращает его маску"""
-    return f"**{account_number[-4:]}"
-
-# print(get_mask_card_number(1234562289606361))
+    try:
+        return f"**{account_number[-4:]}"
+    except TypeError:
+        return 'Account mus be string'
