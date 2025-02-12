f53168272f7db73e0c587c3b07773a1df1f78756
Artem Filippow <a.filippov@qtech.ru>
2025-02-12 19:49:43 +0900

    tests for processing module

---
+++ tests/test_processing.py
@@ -0,0 +1,15 @@
+from typing import List
+import pytest
+from data_for_testing import lists_sort_by_date, lists_filer_by_state
+
+from src.processing import filter_by_state, sort_by_date
+
+
+@pytest.mark.parametrize("data_list, reverse, expected", lists_sort_by_date())
+def test_sort_by_date(data_list: List, reverse: bool, expected: List) -> None:
+    assert sort_by_date(data_list, reverse) == expected
+
+
+@pytest.mark.parametrize("data_list, state, expected", lists_filer_by_state())
+def test_filter_by_state(data_list: List, state: str, expected: List) -> None:
+    assert filter_by_state(data_list, state) == expected
