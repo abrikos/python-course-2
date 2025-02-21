79364f2eaf35581dc70b9e620937673e7faef127
Artem Filippow <a.filippov@qtech.ru>
2025-02-21 19:34:38 +0900

    generators filter_by_currency test

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from typing import List
 
 import pytest
+from tests.data_for_testing import transactions
 
 
 @pytest.fixture
@@ -13,4 +14,74 @@ def dates() -> List:
         ("20.03", f'20.{datetime.now().strftime("%m")}.{datetime.now().year}'),
         ("20-00-12", "Wrong input date"),
         ("text", "Wrong input date"),
+    ]
+
+
+@pytest.fixture
+def transactions_fixture_usd() -> List:
+    return [{
+        "id": 1,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "100",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702"
+    }, {
+        "id": 3,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {
+            "amount": "300",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188"
+    }
+    ]
+
+
+@pytest.fixture
+def transactions_fixture_rur() -> List:
+    return [
+        {
+            "id": 2,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "200",
+                "currency": {
+                    "name": "RUR",
+                    "code": "RUR"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        },
+        {
+            "id": 4,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "400",
+                "currency": {
+                    "name": "RUR",
+                    "code": "RUR"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        }
     ]
---
+++ tests/test_generators.py
@@ -0,0 +1,22 @@
+from typing import List
+
+import pytest
+from data_for_testing import transactions
+
+from src.generators import filter_by_currency
+
+
+# @pytest.mark.parametrize("data_list, currency, expected", data_transactions_by_currency())
+# def test_filter_by_currency(data_list: List, currency: str, expected: List) -> None:
+#    assert filter_by_currency(data_list, currency) == expected
+
+
+def test_transactions_usd(transactions_fixture_usd: List) -> None:
+    iterator = filter_by_currency(transactions, "USD")
+    for fixture in transactions_fixture_usd:
+        assert next(iterator) == fixture
+
+def test_transactions_rur(transactions_fixture_rur: List) -> None:
+    iterator = filter_by_currency(transactions, "RUR")
+    for fixture in transactions_fixture_rur:
+        assert next(iterator) == fixture
