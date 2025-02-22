b339c0e308356ec6aee52a2df545b5bac5eb77cf
Artem Filippow <a.filippov@qtech.ru>
2025-02-22 15:42:04 +0900

    PEP8 formatting

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from typing import List
 
 import pytest
-from tests.data_for_testing import transactions
 
 
 @pytest.fixture
@@ -24,39 +23,30 @@ def transactions_descriptions_fixture() -> List:
         "Перевод со счета на счет",
         "Перевод со счета на счет",
         "Перевод со счета на счет",
-
     ]
+
+
 @pytest.fixture
 def transactions_fixture_usd() -> List:
-    return [{
-        "id": 1,
-        "state": "EXECUTED",
-        "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {
-            "amount": "100",
-            "currency": {
-                "name": "USD",
-                "code": "USD"
-            }
+    return [
+        {
+            "id": 1,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {"amount": "100", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702",
         },
-        "description": "Перевод организации",
-        "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702"
-    }, {
-        "id": 3,
-        "state": "EXECUTED",
-        "date": "2019-04-04T23:20:05.206878",
-        "operationAmount": {
-            "amount": "300",
-            "currency": {
-                "name": "USD",
-                "code": "USD"
-            }
+        {
+            "id": 3,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {"amount": "300", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188",
         },
-        "description": "Перевод со счета на счет",
-        "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188"
-    }
     ]
 
 
@@ -67,30 +57,18 @@ def transactions_fixture_rur() -> List:
             "id": 2,
             "state": "EXECUTED",
             "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "200",
-                "currency": {
-                    "name": "RUR",
-                    "code": "RUR"
-                }
-            },
+            "operationAmount": {"amount": "200", "currency": {"name": "RUR", "code": "RUR"}},
             "description": "Перевод со счета на счет",
             "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
+            "to": "Счет 75651667383060284188",
         },
         {
             "id": 4,
             "state": "EXECUTED",
             "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "400",
-                "currency": {
-                    "name": "RUR",
-                    "code": "RUR"
-                }
-            },
+            "operationAmount": {"amount": "400", "currency": {"name": "RUR", "code": "RUR"}},
             "description": "Перевод со счета на счет",
             "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        }
+            "to": "Счет 75651667383060284188",
+        },
     ]
