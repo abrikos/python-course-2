983c14b30e696810b575081bd93682c0eeff99dd
Artem Filippow <a.filippov@qtech.ru>
2025-02-22 15:41:29 +0900

    fix types

--- src/generators.py
+++ src/generators.py
@@ -1,11 +1,12 @@
-from typing import List
+from typing import Generator, List
 
 
-def filter_by_currency(transactions: List, currency="USD") -> iter:
+def filter_by_currency(transactions: List, currency: str = "USD") -> filter:
     """Фильтрация транзакций по валюте"""
     return filter(lambda t: t["operationAmount"]["currency"]["name"] == currency, transactions)
 
-def transaction_descriptions(transactions: List) ->iter:
+
+def transaction_descriptions(transactions: List) -> Generator:
     """Вывод описания транзакций"""
     for tx in transactions:
         yield tx["description"]
