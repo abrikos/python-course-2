d6dc1513461bd6b41caddadfc5a8851249eec37c
Artem Filippow <a.filippov@qtech.ru>
2025-02-24 11:54:53 +0900

    fix card number generator

--- src/generators.py
+++ src/generators.py
@@ -1,14 +1,13 @@
 from typing import Generator, List
 
 
-def card_number_generator(start: int, end: int) -> List:
+def card_number_generator(start: int, end: int) -> Generator:
     """Генератор номеров карт"""
-    cards = []
     for num in range(start, end + 1):
         x = str(num).rjust(16, "0")
         chunks, chunk_size = len(x), len(x) // 4
-        cards.append(" ".join([x[i : i + chunk_size] for i in range(0, chunks, chunk_size)]))
-    return cards
+        card = " ".join([x[i : i + chunk_size] for i in range(0, chunks, chunk_size)])
+        yield card
 
 
 def filter_by_currency(transactions: List, currency: str = "USD") -> filter:
