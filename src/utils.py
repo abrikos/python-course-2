babc36148de0f784d30f83441beab9649c192031
Artem Filippow <a.filippov@qtech.ru>
2025-03-11 16:34:25 +0900

    logging

--- src/utils.py
+++ src/utils.py
@@ -2,14 +2,20 @@ import json
 from typing import Any
 
 from src.external_api import get_course
+from src.logger import get_logger
+
+log_file = "../logs/utils.log"
+logger = get_logger(log_file)
 
 
 def read_file(file: str) -> str:
     """Чтение фала"""
     try:
-        with open(file) as f:
-            return f.read()
-    except FileNotFoundError:
+        with open(file) as f1:
+            logger.debug(f'Read file success: "{file}"')
+            return f1.read()
+    except FileNotFoundError as e:
+        logger.error(e)
         return "[]"
 
 
@@ -17,11 +23,16 @@ def read_transactions(file: str) -> Any:
     """Чтение транзакций из файла"""
     try:
         res = json.loads(read_file(file))
-        if type(res) == 'list':
+        if type(res) is list:
+            logger.debug("Read txs success")
+            if len(res) == 0:
+                logger.warning(f"Data length: {len(res)}")
             return res
         else:
+            logger.warning(f'"{file}" has no list data type ({type(res)})')
             return []
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as e:
+        logger.error(e)
         return []
 
 
@@ -30,5 +41,10 @@ def transaction_sum(tx: dict) -> float:
     course = 1.0
     if tx["operationAmount"]["currency"]["code"] in ("USD", "EUR"):
         course = get_course(tx["operationAmount"]["currency"]["code"])
+    logger.debug("TX sum calculated")
     return float(tx["operationAmount"]["amount"]) * course
 
+
+# read_transactions("../data/operations2.json")
+# with open(log_file) as f:
+#    print(f.read())
