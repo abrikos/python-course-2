800e5ac231b5a50525d2072cc7ed800969682041
Artem Filippow <a.filippov@qtech.ru>
2025-03-13 14:16:33 +0900

    fix csv test

--- tests/test_csv_excel.py
+++ tests/test_csv_excel.py
@@ -7,18 +7,32 @@ import src.csv_excel
 
 
 def test_csv() -> None:
-    with patch("builtins.open", mock_open(read_data="head\n10;11;12;13;14;15;16;17;18")):
+    mock_data = """id;state;date;amount;currency_name;currency_code;from;to;description
+10;11;12;13;14;15;16;17;18
+20;21;22;23;24;25;26;27;28"""
+    with patch("builtins.open", mock_open(read_data=mock_data)):
         assert src.csv_excel.read_csv("fake_file") == [
             {
                 "amount": "13",
                 "currency_code": "15",
                 "currency_name": "14",
                 "date": "12",
-                "description": "18",
+                "description\n": "18",
                 "from": "16",
                 "id": "10",
                 "state": "11",
                 "to": "17",
+            },
+            {
+                "amount": "23",
+                "currency_code": "25",
+                "currency_name": "24",
+                "date": "22",
+                "description\n": "28",
+                "from": "26",
+                "id": "20",
+                "state": "21",
+                "to": "27",
             }
         ]
 
