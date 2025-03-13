38f999e983aced340ab3fcdfadc7f39b6cb7bcde
Artem Filippow <a.filippov@qtech.ru>
2025-03-13 14:04:53 +0900

    pep 8

--- src/csv_excel.py
+++ src/csv_excel.py
@@ -1,13 +1,27 @@
 import csv
 
 import pandas as pd
-import collections as co
+
 
 def read_csv(file: str) -> list:
     """Read txs from CSV"""
     try:
         with open(file) as f:
-            reader = csv.DictReader(f, delimiter=';', fieldnames=['id','state','date','amount','currency_name','currency_code','from','to','description'])
+            reader = csv.DictReader(
+                f,
+                delimiter=";",
+                fieldnames=[
+                    "id",
+                    "state",
+                    "date",
+                    "amount",
+                    "currency_name",
+                    "currency_code",
+                    "from",
+                    "to",
+                    "description",
+                ],
+            )
             next(reader)
             tx_list = []
             for row in reader:
@@ -22,11 +36,11 @@ def read_xls(file: str):
     """Read txs from Excel"""
     try:
         df = pd.read_excel(file)
-        return df.to_dict('records')
+        return df.to_dict("records")
     except (FileNotFoundError, ValueError) as e:
         print(e)
         return []
 
 
-#print(read_csv("../data/transactions.csv"))
-#print(read_xls("../data/transactions_excel.xlsx"))
+# print(read_csv("../data/transactions.csv"))
+# print(read_xls("../data/transactions_excel.xlsx"))
--- tests/test_csv_excel.py
+++ tests/test_csv_excel.py
@@ -8,15 +8,19 @@ import src.csv_excel
 
 def test_csv() -> None:
     with patch("builtins.open", mock_open(read_data="head\n10;11;12;13;14;15;16;17;18")):
-        assert src.csv_excel.read_csv("fake_file") == [{'amount': '13',
-                                                        'currency_code': '15',
-                                                        'currency_name': '14',
-                                                        'date': '12',
-                                                        'description': '18',
-                                                        'from': '16',
-                                                        'id': '10',
-                                                        'state': '11',
-                                                        'to': '17'}]
+        assert src.csv_excel.read_csv("fake_file") == [
+            {
+                "amount": "13",
+                "currency_code": "15",
+                "currency_name": "14",
+                "date": "12",
+                "description": "18",
+                "from": "16",
+                "id": "10",
+                "state": "11",
+                "to": "17",
+            }
+        ]
 
 
 @patch("pandas.read_excel")
