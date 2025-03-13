8dfe87ebfbaad5c8d414e0acd399ba4788a48238
Artem Filippow <a.filippov@qtech.ru>
2025-03-13 14:00:46 +0900

    fix return data

--- src/csv_excel.py
+++ src/csv_excel.py
@@ -1,13 +1,18 @@
 import csv
 
 import pandas as pd
-
+import collections as co
 
 def read_csv(file: str) -> list:
     """Read txs from CSV"""
     try:
         with open(file) as f:
-            return list(csv.reader(f))
+            reader = csv.DictReader(f, delimiter=';', fieldnames=['id','state','date','amount','currency_name','currency_code','from','to','description'])
+            next(reader)
+            tx_list = []
+            for row in reader:
+                tx_list.append(row)
+            return tx_list
     except (FileNotFoundError, ValueError) as e:
         print(e)
         return []
@@ -16,11 +21,12 @@ def read_csv(file: str) -> list:
 def read_xls(file: str):
     """Read txs from Excel"""
     try:
-        return pd.read_excel(file).values.tolist()
+        df = pd.read_excel(file)
+        return df.to_dict('records')
     except (FileNotFoundError, ValueError) as e:
         print(e)
         return []
 
 
-print(read_csv("../data/transactions.csv"))
-# print(read_xls("../data/transactions_excel.xlsx"))
+#print(read_csv("../data/transactions.csv"))
+print(read_xls("../data/transactions_excel.xlsx"))
--- tests/test_csv_excel.py
+++ tests/test_csv_excel.py
@@ -7,13 +7,21 @@ import src.csv_excel
 
 
 def test_csv() -> None:
-    with patch("builtins.open", mock_open(read_data="a;bc")):
-        assert src.csv_excel.read_csv("fake_file") == [["a;bc"]]
+    with patch("builtins.open", mock_open(read_data="head\n10;11;12;13;14;15;16;17;18")):
+        assert src.csv_excel.read_csv("fake_file") == [{'amount': '13',
+                                                        'currency_code': '15',
+                                                        'currency_name': '14',
+                                                        'date': '12',
+                                                        'description': '18',
+                                                        'from': '16',
+                                                        'id': '10',
+                                                        'state': '11',
+                                                        'to': '17'}]
 
 
 @patch("pandas.read_excel")
 def test_excel(mock_read_excel: Any) -> None:
-    mock_data = pandas.DataFrame({"id": [1, 2, 3], "value": [6, 7, 8]})
+    mock_data = pandas.DataFrame([{"id": 1, "state": 2}])
     mock_read_excel.return_value = mock_data
     res = src.csv_excel.read_xls("fake_file")
-    assert [[1, 6], [2, 7], [3, 8]] == res
+    assert [{"id": 1, "state": 2}] == res
