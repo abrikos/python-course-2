e57e32f817d2065d02b47539a5ed771f15f4e091
Artem Filippow <a.filippov@qtech.ru>
2025-03-14 20:03:49 +0900

    stat of transactions

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,12 @@
-import json
-from typing import Any
-
 import csv
+import json
+import re
+from collections import Counter
 
 import pandas as pd
+
 from src.external_api import get_course
 from src.logger import get_logger
-import re
 
 log_file = "../logs/utils.log"
 logger = get_logger(log_file)
@@ -32,8 +32,8 @@ def transaction_sum(tx: dict) -> float:
     return float(tx["operationAmount"]["amount"]) * course
 
 
-def read_transactions_json(file: str) -> Any:
-    """Чтение транзакций из файла"""
+def read_transactions_json(file: str) -> list:
+    """Чтение транзакций из файла JSON"""
     try:
         res = json.loads(read_file(file))
         if type(res) is list:
@@ -53,13 +53,8 @@ def read_transactions_csv(file: str) -> list:
     """Read txs from CSV"""
     try:
         with open(file) as f:
-            first_line = f.readline().strip().split(';')
-            reader = csv.DictReader(
-                f,
-                delimiter=";",
-                fieldnames=first_line,
-                lineterminator='\n'
-            )
+            first_line = f.readline().strip().split(";")
+            reader = csv.DictReader(f, delimiter=";", fieldnames=first_line, lineterminator="\n")
             tx_list = []
             for row in reader:
                 tx_list.append(row)
@@ -69,12 +64,12 @@ def read_transactions_csv(file: str) -> list:
         return []
 
 
-def read_transactions_xls(file: str):
+def read_transactions_xls(file: str) -> list:
     """Read txs from Excel"""
     try:
         df = pd.read_excel(file)
         keys = df.to_dict("records")[0].keys()
-        return list(filter(lambda x: all(key in x for key in keys), df.to_dict("records")))
+        return list(filter(lambda x: all(key in x for key in keys) and x["id"] > 0, df.to_dict("records")))
     except (FileNotFoundError, ValueError) as e:
         print(e)
         return []
@@ -82,13 +77,13 @@ def read_transactions_xls(file: str):
 
 def read_transactions_by_ext(ext: str) -> list:
     """Read transactions from files by extension"""
-    file = '../data/transactions.' + ext
+    file = "../data/transactions." + ext
     match ext:
-        case 'csv':
+        case "csv":
             return read_transactions_csv(file)
-        case 'json':
+        case "json":
             return read_transactions_json(file)
-        case 'xlsx':
+        case "xlsx":
             return read_transactions_xls(file)
         case _:
             return []
@@ -96,16 +91,9 @@ def read_transactions_by_ext(ext: str) -> list:
 
 def search_txs_by_desc(transactions: list, search: str) -> list:
     """Search txs by desc"""
-    return list(filter(lambda x: re.search(str(search), x['description'], flags=re.IGNORECASE), transactions))
+    return list(filter(lambda x: re.search(str(search), x["description"] if x["description"] else '', flags=re.IGNORECASE), transactions))
 
 
-def search_txs_by_state(transactions: list, states: list) -> dict:
+def count_txs_by_type(transactions: list, states: list) -> dict:
     """Search txs by state"""
-    try:
-        states_upper = [x.upper() for x in states]
-        valid_statuses = ['EXECUTED', 'CANCELED', 'PENDING']
-        if len(set(states_upper) & set(valid_statuses)) == 0:
-            return {'error': 'wrong_state'}
-        return {'found': list(filter(lambda x: x['state'] in states_upper, transactions))}
-    except TypeError as e:
-        return {'error': 'type_error'}
+    return dict(Counter(map(lambda d: d["state"], filter(lambda d: d["state"] in states, transactions))))
